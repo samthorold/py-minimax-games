54364122a19a9cee6ade5acc027482858c7f6050
Sam Thorold <sam.thorold@gmail.com>
2023-10-22 10:21:13 +0100

    Play games

--- src/minimax_games/games/wordle/game.py
+++ src/minimax_games/games/wordle/game.py
@@ -209,16 +209,3 @@ cli.add_argument("--vocabulary")
 cli.add_argument("--log-level", default="WARNING")
 cli.add_argument("--interactive-guess", action="store_true")
 cli.add_argument("--interactive-score", action="store_true")
-
-
-if __name__ == "__main__":
-    print("=== PyWordle ===")
-    args = WordleArgs.from_argument_parser(cli)
-    logging.basicConfig(level=args.log_level)
-    main(
-        truth=args.truth,
-        vocabulary=args.vocabulary,
-        guesser=args.guesser,
-        scorer=args.scorer,
-    )
-    print(args.truth)
